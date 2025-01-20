0d0a1d39e9b9d2887a0d42a5ecbf90202414a7a7
Christian Cham <christianfintacham@gmail.com>
2025-01-21 00:20:08 +0800

    Base React Tailwind Application

---
+++ src/router.jsx
@@ -0,0 +1,13 @@
+import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const AppRouter = () => {
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  </Router>;
+};
+
+export default AppRouter;
